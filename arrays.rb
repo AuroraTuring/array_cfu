4e89a388046cf844a7237ec25a4cd39744b6cc71
Aurora Sanderson <AuroraTuring@gmail.com>
2023-08-31 12:10:33 -0600

    Add questions

--- arrays.rb
+++ arrays.rb
@@ -5,16 +5,16 @@ sick = [true, false, true, false]
 
 
 # Calling this method returns the last element to you. 
-temperatures.pop 
+puts temperatures.pop 
 
 # Calling this method adds an element to the end of the variable.
-ages.push 25
+puts ages.push 25
 
 # Calling this method adds an element to the beginning of the variable.
-friends.shift
+puts friends.shift
 
 # Calling this method returns the first element to you. 
-ages.unshift
+puts ages.unshift
 
 
 # Index Positions:
@@ -37,4 +37,4 @@ puts ages.last
 puts temperatures.length
 
 # Checks if you are sick.
-is_sick 
+puts sick
---
+++ self-evaluation.md
@@ -0,0 +1,10 @@
+# Questions: 
+
+- **How confident do you feel with the content on Arrays so far?**
+Text. 
+
+- **Is there any additional learning you need or want to do before moving to the next lesson?**
+Text. 
+
+- **What questions do you have about Arrays?**
+Text. 

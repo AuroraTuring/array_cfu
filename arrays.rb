355d1c25af0531e590ebc62a4a7cc39d937e4d9e
Aurora Sanderson <AuroraTuring@gmail.com>
2023-08-30 22:30:27 -0600

    Add questions and answers

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,35 @@
 friends = ["Akane", "Miles", "Jessie", "Shandra"]
 ages = [30, 31, 32, 33]
-temperature = [98.6, 99.5, 100.4, 101.3]
-is_sick = [true, false, true, false]
+temperatures = [98.6, 99.5, 100.4, 101.3]
+sick = [true, false, true, false]
 
+
+# Calling this method returns the last element to you. 
+temperatures.pop 
+
+# Calling this method adds an element to the end of the variable.
+ages.push 25
+
+# text
+shift
+
+# text 
+unshift
+
+
+
+
+
+
+# Additional Pracctice:
+# This will call the name of the first element. 
+puts friends.first 
+
+# This will call the age of the last element.
+puts ages.last
+
+# Lists how many temperature floats exist.
+puts temperatures.length
+
+# Checks if you are sick.
+is_sick 
